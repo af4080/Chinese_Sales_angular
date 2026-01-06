c40ef86cbe408beaec2483e8ca986d82e68d8ace
אביגיל פישר <329228977@mbydomain>
2026-01-06 15:23:05 +0200

    add login

--- src/app/auth/components/register/register.ts
+++ src/app/auth/components/register/register.ts
@@ -16,30 +16,28 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angula
 export class Register {
 
 
-    messageService = inject(MessageService);
+   // messageService = inject(MessageService);
 
-    exampleForm: FormGroup;
+    fb = inject(FormBuilder);
 
     formSubmitted = false;
 
-    constructor(private fb: FormBuilder) {
-        this.exampleForm = this.fb.group({
+   
+        registerForm = this.fb.group({
             username: ['', Validators.required],
-            email: ['', [Validators.required, Validators.email]]
-        });
-    }
+            email: ['', [Validators.required, Validators.email]]});
 
     onSubmit() {
         this.formSubmitted = true;
-        if (this.exampleForm.valid) {
-            this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Form Submitted', life: 3000 });
-            this.exampleForm.reset();
+        if (this.registerForm.valid) {
+          //  this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Form Submitted', life: 3000 });
+            this.registerForm.reset();
             this.formSubmitted = false;
         }
     }
 
     isInvalid(controlName: string) {
-        const control = this.exampleForm.get(controlName);
+        const control = this.registerForm.get(controlName);
         return control?.invalid && (control.touched || this.formSubmitted);
     }
 }
