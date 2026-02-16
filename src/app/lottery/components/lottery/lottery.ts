3b64997efa1577964302594d7335f7efe7acbccf
af4080 <a0548564080@gmail.com>
2026-02-16 23:12:34 +0200

    design lottery & menu

--- src/app/lottery/components/lottery/lottery.ts
+++ src/app/lottery/components/lottery/lottery.ts
@@ -2,10 +2,18 @@ import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { LotteryService } from '../../servieces/lottery.serviece';
 import { CommonModule } from '@angular/common';
 import { ReadGift } from '../../../gifts/models/gift.model';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import confetti from 'canvas-confetti';
 
 @Component({
   selector: 'app-lottery',
-  imports: [CommonModule],
+  imports: [CommonModule,
+    CardModule,
+    ButtonModule,
+    RippleModule
+  ],
   templateUrl: './lottery.html',
   styleUrl: './lottery.scss',
 })
@@ -15,31 +23,33 @@ export class Lottery {
   lotteryService = inject(LotteryService);
   isLoading = false;
   message = '';
-  winners:any[] = [];
-  cdr= inject(ChangeDetectorRef);
+  winners: any[] = [];
+  afterLottery=false;
+  beforeLottery=true;
+  cdr = inject(ChangeDetectorRef);
   ngOnInit() {
     this.loadWinners();
   }
 
-loadWinners() {
-  this.lotteryService.getallwinners().subscribe({
-    next: (res: any[]) => {  
-      this.winners = res.map((winner: any) => 
-        `זוכה: ${winner.winnerName}, מתנה: ${winner.giftName}`
-      );
-       if(this.winners.length > 0) {
-        this.isLoading =true;
+  loadWinners() {
+    this.lotteryService.getallwinners().subscribe({
+      next: (res: any[]) => {
+        this.winners = res.map((winner: any) =>
+          `זוכה: ${winner.winnerName}, מתנה: ${winner.giftName}`
+        );
+        if (this.winners.length > 0) {
+          this.isLoading = true;
+        }
+        console.log('Winners loaded successfully');
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        this.message = 'שגיאה בטעינת הזוכים';
+        console.error('Error:', err);
+        // עדכון התצוגה במקרה של שגיאה
       }
-      console.log('Winners loaded successfully');
-       this.cdr.detectChanges();
-    },
-    error: (err) => {
-      this.message = 'שגיאה בטעינת הזוכים';
-      console.error('Error:', err);
-      // עדכון התצוגה במקרה של שגיאה
-    }
-  });
-}
+    });
+  }
 
 
   runAllLotteries() {
@@ -52,7 +62,8 @@ loadWinners() {
           this.message = 'ההגרלה הסתיימה בהצלחה!';
           this.isLoading = false;
           this.loadWinners();
-           this.cdr.detectChanges();
+          this.celebrate();
+          this.cdr.detectChanges();
         },
         error: (err) => {
           console.error('Server Error:', err.error);
@@ -92,4 +103,31 @@ loadWinners() {
       });
     }
   }
+  celebrate() {
+    const duration = 3 * 1000;
+    const end = Date.now() + duration;
+
+    (function frame() {
+      confetti({
+        particleCount: 3,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0 },
+        colors: ['#712059', '#D4AF37', '#F1D382']
+      });
+      confetti({
+        particleCount: 3,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1 },
+        colors: ['#712059', '#D4AF37', '#F1D382']
+      });
+
+      if (Date.now() < end) {
+        requestAnimationFrame(frame);
+      }
+    }());
+  }
+
 }
+
